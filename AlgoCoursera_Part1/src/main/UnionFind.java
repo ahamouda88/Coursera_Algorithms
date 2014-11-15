84d3a29461d703a45263506c03c8e904c84069b8
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-11-14 19:35:21 -0600

    Adding some implementation for the UnionFind class

--- AlgoCoursera_Part1/src/main/UnionFind.java
+++ AlgoCoursera_Part1/src/main/UnionFind.java
@@ -2,4 +2,17 @@ package main;
 
 public class UnionFind {
 
+	private int value;
+	
+	public UnionFind(int value){
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
 }
