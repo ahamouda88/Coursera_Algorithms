3864efec1b5e2ca788fb277174dee43c39f3b806
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-11-14 20:43:33 -0600

    Add an improvement for the Quick Union techniques.

---
+++ AlgoCoursera_Part1/src/unionfind/techniques/WeightedQuickUnion.java
@@ -0,0 +1,37 @@
+package unionfind.techniques;
+
+public class WeightedQuickUnion {
+	private int[] numbers;
+	private int[] sizes;
+
+	public WeightedQuickUnion(int n) {
+		numbers = new int[n];
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = i;
+			sizes[i] = 1;
+		}
+	}
+
+	public boolean isConnected(int x, int y) {
+		return getRoot(x) == getRoot(y);
+	}
+
+	public void union(int x, int y) {
+		int xId = getRoot(x);
+		int yId = getRoot(y);
+		if(sizes[xId] > sizes[yId]){
+			numbers[yId] = xId;
+			sizes[xId] += sizes[yId];
+		}else{
+			numbers[xId] = yId;
+			sizes[yId] += sizes[xId];
+		}
+	}
+
+	private int getRoot(int x) {
+		while (x != numbers[x]) {
+			x = numbers[x];
+		}
+		return x;
+	}
+}
