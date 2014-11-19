38d8350cd81c594a806a934cf52b80357c8cc3ec
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-11-18 18:52:31 -0600

    Testing the weightedquickunion algorithm to solve dynamic connectivity problem.

---
+++ AlgoCoursera_Part1/src/week1/test/UnionFindTest.java
@@ -0,0 +1,22 @@
+package week1.test;
+
+import week1.unionfind.WeightedQuickUnion;
+
+public class UnionFindTest {
+
+	public static void main(String[] args) {
+		
+		WeightedQuickUnion wqu = new WeightedQuickUnion(10);
+		wqu.union(4, 3);
+		wqu.union(3, 8);
+		wqu.union(6, 5);
+		wqu.union(9, 4);
+		wqu.union(2, 1);
+		wqu.union(5, 0);
+		wqu.union(7, 2);
+		System.out.println("Is 0 connected to 1: "+wqu.isConnected(0, 1));
+		System.out.println("Is 7 connected to 3: "+wqu.isConnected(7, 3));
+		System.out.println("Is 4 connected to 8: "+wqu.isConnected(4, 8));
+	}
+
+}
