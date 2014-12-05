6e58acdca6593d5f5ec9b7d53b1e866d11a0ace0
ahamouda88 <ahmed.hamouda8888@gmail.com>
2014-12-04 18:03:45 -0600

    Adding Shuffle Sort algorithm

---
+++ AlgoCoursera_Part1/src/week2/sorting/ShuffleSort.java
@@ -0,0 +1,33 @@
+package week2.sorting;
+
+import java.util.Random;
+
+/**
+ * A class to shuffle an array, using Knuth Shuffle Algorithm. 
+ * It takes linear time to shuffle an array.
+ * 
+ * @author ahamouda
+ * 
+ */
+public class ShuffleSort {
+	
+	public static void shuffle(int[] a) {
+		int n = a.length;
+		Random random = new Random();
+		for (int i = 0; i < n; i++) {
+			int r = random.nextInt(i + 1); 
+			swap(a, i, r);
+		}
+	}
+
+	/**
+	 * A Method to swap two elements in an array.
+	 * 
+	 * @param a
+	 */
+	private static void swap(int[] a, int pos1, int pos2) {
+		int temp = a[pos1];
+		a[pos1] = a[pos2];
+		a[pos2] = temp;
+	}
+}
